6cb176a1d375a02ca22f57ff126641da7e192e49
Adam Owada <adamowada@gmail.com>
2020-06-11 22:19:08 -0700

    completed classes and dunder methods, need to write tests

--- pythonic_garage_band/pythonic_garage_band.py
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -12,20 +12,44 @@ class Musician:
     def play_solo(self):
         return self.name + "'s solo sounds like this: " + self.solo
 
+    def __repr__(self):
+        return "Musician('{}', '{}')".format(self.name, self.solo)
+
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.solo)
+
 
 class Guitarist(Musician):
 
     instrument = "Guitar"
+
+    def __repr__(self):
+        return "Guitarist('{}', '{}')".format(self.name, self.solo)
+
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.solo)
 
 
 class Bassist(Musician):
 
     instrument = "Bass"
 
+    def __repr__(self):
+        return "Bassist('{}', '{}')".format(self.name, self.solo)
+
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.solo)
+
 
 class Drummer(Musician):
 
     instrument = "Drums"
+
+    def __repr__(self):
+        return "Drummer('{}', '{}')".format(self.name, self.solo)
+
+    def __str__(self):
+        return '{}: {}'.format(self.name, self.solo)
 
 
 class Band:
@@ -45,14 +69,38 @@ class Band:
         for i in self.members:
             print(i.play_solo())
 
+    def __repr__(self):
+        return 'Band("{}", "{}")'.format(self.name, self.members)
+
+    def __str__(self):
+        string = self.name + ': '
+        for i in self.members:
+            string += i.name + ' '
+        return string
+
     @classmethod
     def to_list(cls):
         for i in cls.prev_bands:
             return i
 
 
-guitar_1 = Guitarist("Adam", "Brown chicken brown cow!")
+guitar_1 = Guitarist("Spike", "Brown chicken brown cow!")
+guitar_2 = Guitarist("Mike", "Bwaaa ri ri bwaaaaaa!")
+guitar_3 = Guitarist("Matt", "Zhweee bap baaa!")
 
-print(guitar_1.name)
-print(guitar_1.get_instrument())
-print(guitar_1.play_solo())
+bass_1 = Bassist("Adam", "da do dee do dum de dooo")
+bass_2 = Bassist("Phill", "bwawawawawaaaaaaa")
+bass_3 = Bassist("Al", "doe ray mi fa so la ti doe")
+
+drum_1 = Drummer("Dave", "ba-du-bi-do-ba-ba-ba-ba-ba-tshhh")
+drum_2 = Drummer("Ringo", "tap tap tap tap ba daaa")
+drum_3 = Drummer("Travis", "tst tst kats-it tst tst kats-is")
+
+band_1 = Band("Warm October", [guitar_2, guitar_3, bass_1, bass_2, drum_1])
+
+
+band_1.play_solos()
+Band.to_list()
+print(repr(bass_1))
+print(repr(band_1))
+print(str(band_1))
